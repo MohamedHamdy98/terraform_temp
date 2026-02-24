e8b4aa733aa4b4c8ecd692a27e9f46b64f57078f
MohammedHamdy98 <dodo222.mohamed@gmail.com>
2026-02-25 01:30:35 +0200

    refactor: clean up formatting in main.tf and providers.tf adding: statefile on s3 storage and create the state locking on it.

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 
 
 resource "aws_instance" "terraform_test" {
-  ami           = var.instance_ami
-  instance_type = local.prefix_instance_type
-
-  tags = {
+ami           = var.instance_ami
+instance_type = local.prefix_instance_type
+tags = {
     Name = "terraform_test"
-  }
+    }
 }
+
 
--- providers.tf
+++ providers.tf
@@ -6,8 +6,15 @@ terraform {
       version = "6.33.0"
     }
   }
+  backend "s3" {
+    bucket = AWS_S3_BUCJET_NAME
+    key    = "prod/state"   
+    region = "eu-west-2"
+    use_lockfile = true
+    }
 }
 
 provider "aws" {
-
+  region = "eu-west-2"
 }
+
